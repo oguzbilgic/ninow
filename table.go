1b55aa0a959458eeca969ab25460baf68d819a9c
Oguz Bilgic <fisyonet@gmail.com>
2014-02-28 20:14:46 -0500

    Create utils.go file

--- table.go
+++ table.go
@@ -5,20 +5,7 @@ import (
 	"database/sql"
 	"reflect"
 	"strings"
-	"unicode"
 )
-
-func csv(values []string) string {
-	list := ""
-	for i, value := range values {
-		if i != (len(values) - 1) {
-			list += value + ", "
-		} else {
-			list += value
-		}
-	}
-	return list
-}
 
 type Table struct {
 	db    *sql.DB
@@ -48,48 +35,7 @@ func TableFor(row interface{}, db *sql.DB) *Table {
 func (t *Table) Select(id int) (interface{}, error) {
 	row := t.db.QueryRow("SELECT "+csv(t.columns)+" FROM "+t.name+" WHERE id=?", id)
 	value := reflect.New(t.rtype)
-	indirectValue := reflect.Indirect(value)
+	err := row.Scan(fieldPointers(t.fields, value)...)
 
-	var pointers []interface{}
-	for _, field := range t.fields {
-		fieldValue := indirectValue.FieldByName(field)
-		addr := fieldValue.Addr()
-		pointers = append(pointers, addr.Interface())
-	}
-
-	err := row.Scan(pointers...)
-	if err != nil {
-		return nil, err
-	}
-
-	return value.Interface(), nil
-}
-
-func fieldNameToColumnName(fieldName string) string {
-	//TODO add more edge cases to conform idiomatically
-	edgeCases := []string{"ID", "URL"}
-
-	for _, keyword := range edgeCases {
-		fieldName = fixEdgeFieldName(fieldName, keyword)
-	}
-
-	columnName := strings.ToLower(string(fieldName[0]))
-	for _, char := range []rune(fieldName[1:]) {
-		if unicode.IsUpper(char) {
-			columnName = columnName + "_" + strings.ToLower(string(char))
-		} else {
-			columnName = columnName + string(char)
-		}
-	}
-	return columnName
-}
-
-func fixEdgeFieldName(fieldName string, keyword string) string {
-	if !strings.Contains(fieldName, keyword) {
-		return fieldName
-	}
-	weedle := strings.Index(fieldName, keyword)
-	keywordLength := len(keyword)
-	return fieldName[0:weedle+1] + strings.ToLower(fieldName[weedle+1:weedle+keywordLength]) + fieldName[weedle+keywordLength:len(fieldName)]
-
+	return value.Interface(), err
 }
---
+++ utils.go
@@ -0,0 +1,61 @@
+package ninow
+
+import (
+	"reflect"
+	"strings"
+	"unicode"
+)
+
+func csv(values []string) string {
+	list := ""
+	for i, value := range values {
+		if i != (len(values) - 1) {
+			list += value + ", "
+		} else {
+			list += value
+		}
+	}
+	return list
+}
+
+func fieldPointers(fields []string, value reflect.Value) []interface{} {
+	indirectValue := reflect.Indirect(value)
+
+	var pointers []interface{}
+	for _, field := range fields {
+		fieldValue := indirectValue.FieldByName(field)
+		addr := fieldValue.Addr()
+		pointers = append(pointers, addr.Interface())
+	}
+
+	return pointers
+}
+
+func fieldNameToColumnName(fieldName string) string {
+	//TODO add more edge cases to conform idiomatically
+	edgeCases := []string{"ID", "URL"}
+
+	for _, keyword := range edgeCases {
+		fieldName = fixEdgeFieldName(fieldName, keyword)
+	}
+
+	columnName := strings.ToLower(string(fieldName[0]))
+	for _, char := range []rune(fieldName[1:]) {
+		if unicode.IsUpper(char) {
+			columnName = columnName + "_" + strings.ToLower(string(char))
+		} else {
+			columnName = columnName + string(char)
+		}
+	}
+	return columnName
+}
+
+func fixEdgeFieldName(fieldName string, keyword string) string {
+	if !strings.Contains(fieldName, keyword) {
+		return fieldName
+	}
+	weedle := strings.Index(fieldName, keyword)
+	keywordLength := len(keyword)
+	return fieldName[0:weedle+1] + strings.ToLower(fieldName[weedle+1:weedle+keywordLength]) + fieldName[weedle+keywordLength:len(fieldName)]
+
+}
